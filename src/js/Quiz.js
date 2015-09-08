94df1122c26de84d857145371ac1df29c12028e8
Chris Sheppard <github@chrissheppard.co.uk>
2015-09-08 21:43:01 +0100

    Refactoring in to Quiz class

---
+++ src/js/Quiz.js
@@ -0,0 +1,106 @@
+var _ = require("lodash");
+
+function Quiz(items) {
+    this.items = items;
+    this.recipeItem = {
+        img: "recipe_lg.png",
+        localized_name: "Recipe",
+        name: "recipe",
+        recipe: 1,
+        secret_shop: 0,
+        side_shop: 0
+    };
+
+    // The item pool used in the round
+    this.roundItems = [];
+
+    // Possible items for the quiz item
+    this.possibleItems = this.getPossibleItems();
+
+    // Possible components to make
+    this.possibleComponents = this.getPossibleComponents();
+}
+
+Quiz.prototype.getPossibleItems = function () {
+    return _.chain(this.items)
+        .filter(function (item) {
+            // Only items that are created
+            return item.created;
+        }).each(function (item) {
+            // Change all component recipes to be called recipe
+            if (item.components) {
+                _.each(item.components, function (component, n) {
+                    item.components[n] = component.replace(/^recipe(_.+)$/, "recipe");
+                });
+            }
+        })
+        .value();
+};
+
+Quiz.prototype.getPossibleComponents = function () {
+    return _.chain(this.items)
+        .reduce(function (components, item) {
+            // Get all the components as a single array
+            if (item.components) {
+                return components.concat(item.components);
+            }
+            return components;
+        }, [])
+        .remove(function (component) {
+            // Remove recipes
+            return !component.match(/^recipe(_.+)$/);
+        })
+        .uniq()
+        .value();
+};
+
+// Initialize the quiz
+Quiz.prototype.start = function () {
+    // Randomize the item pool
+    this.roundItems = _.shuffle(this.possibleItems);
+};
+
+Quiz.prototype.getRandomComponent = function () {
+    return this.possibleComponents[_.random(this.possibleComponents.length - 1)];
+};
+
+Quiz.prototype.initRound = function () {
+    var NUM_COMPONENTS = 8;
+    var round  = {};
+
+    // Get the item to make
+    round.item = this.roundItems.shift();
+
+    // Randomly choose some components
+    round.choices = _.chain(round.item.components)
+        .clone()
+        .remove(function (component) {
+            return component !== "recipe";
+        })
+        .value();
+
+    while (round.choices.length < NUM_COMPONENTS) {
+        round.choices.push(this.getRandomComponent());
+    }
+
+    round.choices = _.chain(round.choices)
+        .shuffle()
+        .reduce(function (components, itemName) {
+            // Get actual items for componentChoices
+            var item = _.findWhere(this.items, {name: "item_" + itemName});
+            if (item) {
+                components.push(item);
+            }
+            return components;
+        }, [], this)
+        .value();
+
+
+    // Always add recipe as last item
+    round.choices.push(this.recipeItem);
+
+    return round;
+};
+
+module.exports = Quiz;
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,106 +1,31 @@
 var _ = require("lodash");
 var swig = require("swig");
 
+var Quiz = require("./Quiz");
+
 var items = require("../../dist/items.json");
 
 var itemTemplate = require("../template/partial/item.html");
 
-// Possible items for the quiz item
-var possibleItems = _.chain(items)
-    .filter(function(item) {
-        // Only items that are created
-        return item.created;
-    }).each(function(item) {
-        // Change all component recipes to be called recipe
-        if (item.components) {
-            _.each(item.components, function(component, n) {
-                item.components[n] = component.replace(/^recipe(_.+)$/, "recipe");
-            });
-        }
-    })
-    .value();
 
-// Find a recipe item
-var recipeItem = _.chain(items)
-    .find(function (item) {
-        return item.name.match(/^item_recipe(_.+)$/);
-    })
-    .tap(function (item) {
-        item.name = "recipe";
-        item.localized_name = "Recipe";
-        delete item.cost;
-        delete item.id;
-    })
-    .value();
-
-// Possible components to make
-var possibleComponents = _.chain(items)
-    .reduce(function(components, item) {
-        // Get all the components as a single array
-        if (item.components) {
-            return components.concat(item.components);
-        }
-        return components;
-    }, [])
-    .remove(function(component) {
-        // Remove recipes
-        return !component.match(/^recipe(_.+)$/);
-    })
-    .uniq()
-    .value();
-
+// Create the quiz
+var quiz = new Quiz(items);
 
 console.log("Total Items: ", items.length);
-console.log("Possible Buildable Items:", possibleItems.length);
-console.log("Possible Components:", possibleComponents.length);
+console.log("Possible Buildable Items:", quiz.possibleItems.length);
+console.log("Possible Components:", quiz.possibleComponents.length);
 
+quiz.start();
 
+var round = quiz.initRound();
 
-// Round to pick item
-var roundItems = _.shuffle(possibleItems);
-var itemToMake = roundItems[0];
-
-// Randomly choose some components
-var componentChoices = _.chain(itemToMake.components)
-    .clone()
-    .remove(function (component) {
-        return component !== "recipe";
-    })
-    .value();
-var numComponents = 8;
-
-console.log("initial choices", componentChoices);
-
-while (componentChoices.length < numComponents) {
-    var item = possibleComponents[_.random(possibleComponents.length - 1)];
-    componentChoices.push(item);
-}
-
-componentChoices = _.chain(componentChoices)
-    .shuffle()
-    .reduce(function (components, itemName) {
-        // Get actual items for componentChoices
-        var item = _.findWhere(items, {name: "item_" + itemName});
-        if (item) {
-            components.push(item);
-        }
-        return components;
-    }, [])
-    .value();
-
-
-// Always add recipe
-componentChoices.push(recipeItem);
-
-console.log("Make Item: ", roundItems[0].localized_name);
-console.log("Components: ", roundItems[0].components);
-console.log("Choices: ", componentChoices);
+console.log("Make Item: ", round.item.localized_name);
+console.log("Components: ", round.item.components);
+console.log("Choices: ", round.choices);
 
 // Render the item
 var html = swig.render(itemTemplate, {
-    locals: {
-        item: roundItems[0]
-    }
+    locals: round
 });
 
 document.querySelector("#main").innerHTML = html;

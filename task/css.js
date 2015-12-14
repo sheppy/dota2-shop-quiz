35ac4ada0e0b6655c24cb795b241a1e01e61bd73
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-12-14 17:13:41 +0000

    Adding normalize css

--- task/css.js
+++ task/css.js
@@ -1,30 +1,29 @@
 import path from "path";
 import gulp from "gulp";
 import gulpLoadPlugins from "gulp-load-plugins";
-import cssAutoprefixer from "autoprefixer";
-import cssNested from "postcss-nested";
-import cssPartialImport from "postcss-partial-import";
-import cssSimpleVars from "postcss-simple-vars";
-import cssScss from "postcss-scss";
-import cssComments from "postcss-discard-comments";
-import cssReporter from "postcss-reporter";
+import autoprefixer from "autoprefixer";
 import styleLint from "stylelint";
 import styleLintConfig from "../stylelint.config";
 import colorGuard from "colorguard";
 import config from "./config";
 
 const plugins = gulpLoadPlugins();
+const postCss = gulpLoadPlugins({
+    pattern: ["postcss-*", "postcss.*"],
+    replaceString: /^postcss(-|\.)/
+});
 
 gulp.task("css", ["lint-css"], function () {
     let cssProcessors = [
-        cssPartialImport({
+        postCss.normalize(),
+        postCss.partialImport({
             extension: "scss"
         }),
-        cssNested(),
-        cssComments(),
-        cssSimpleVars(),
-        cssAutoprefixer({ browsers: ["last 1 version"] }),
-        cssReporter({
+        postCss.nested(),
+        postCss.discardComments(),
+        postCss.simpleVars(),
+        autoprefixer({ browsers: ["last 1 version"] }),
+        postCss.reporter({
             clearMessages: true
         })
     ];
@@ -32,10 +31,10 @@ gulp.task("css", ["lint-css"], function () {
     return gulp
         .src([
             path.join(config.dir.src, config.dir.scss, config.glob.scss),
-            "!" + path.join(config.dir.src, config.dir.scss, config.glob.scssPartial)
+            "!" + config.glob.scssPartial
         ])
         .pipe(plugins.plumber())
-        .pipe(plugins.postcss(cssProcessors, { syntax: cssScss }))
+        .pipe(plugins.postcss(cssProcessors, { syntax: postCss.scss }))
         .pipe(plugins.rename({
             extname: ".css"
         }))
@@ -47,12 +46,12 @@ gulp.task("lint-css", function () {
     return gulp.src(path.join(config.dir.src, config.dir.scss, config.glob.scss))
         .pipe(plugins.postcss([
             styleLint(styleLintConfig),
-            cssPartialImport({
+            postCss.partialImport({
                 extension: "scss"
             }),
             colorGuard(),
-            cssReporter({
+            postCss.reporter({
                 clearMessages: true
             })
-        ], { syntax: cssScss }))
+        ], { syntax: postCss.scss }))
 });
